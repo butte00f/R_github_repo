60a72e0aa16c9f9ab87ca0673fb7da2d5d09708c
frank.rothenberger <frank.butterich@web.de>
2019-07-24 12:26:08 +0200

    Kapitel 3 started

--- Kapitel_2.R
+++ Kapitel_2.R
@@ -1,15 +1,35 @@
 ##################    Kapitel 2   ############################
 ############### Workflow Grundlagen ##########################
 
-#Übung1
-#geom_line, geom_smooth
+#Grundlagen der Codierung
 
-#Übung2 (tippfehler)
-ggplot(data = mpg, mapping = aes(x = displ, y = hwy, color = drv)) +
-  geom_point() +
-  geom_smooth(se = FALSE)
+#Taschenrechner
+1 / 200 * 30
+(59 +73 +2) / 3
+sin(pi / 2)
+
+# Zuweisungsoperator <-
+a <- 3 * 4
+a  # Ausgabe des Wertes in Console
+r_real <- 2.5  # Kommawert (double)
+r_rocks <- 2 ^ 3 # Exponent
+t_text <- "Hello World"  # String
+
+#Funktionen aufrufen
+seq10 <- seq(1,10)
+seq_calc <- seq(1,10,length.out = 5)
+seq_calc
+
+#Übung 1
+my_variable <- 10; my_variable #variable existiert noch nicht und ist falsch geschrieben
+
+#Übung 2
+library(tidyverse)
+
+ggplot(data = mpg) + geom_point(mapping = aes(x = displ, y = hwy))
 
 filter(mpg, cyl == 8)
-filter(diamonds, carat > 3)
-#Übung3
-# Alle Shortcuts werden angezeigt
+filter(diamonds, carat > 3) # diamond->s
+
+#Übung 3
+# Alt + Shift + K -> Anzeige aller Keyboard shortcuts
---
+++ Kapitel_3.R
@@ -0,0 +1,90 @@
+##################    Kapitel 3   ############################
+############# Datentransformation mit dplyr  #################
+?dplyr
+dplyr # dplyr ist ein Paket mit funktionen und Daten zur Transformation,
+      # herausholen benötigter Daten aus Datensätzen
+
+### Voraussetzungen ###
+
+library(nycflights13)
+library(tidyverse)
+flights
+
+### Datentypen ###
+#<int> = integer , <dbl> = double, <chr> = Character/String, <dttm> = time
+# weitere Datentypen
+
+# <lgl> = logische Typen (TRUE or FALSE)
+# <fctr> = Faktoren, kategoriale Variablen mit festen möglichen Werten
+# <date> = Datum
+
+### dplyr Grundlagen
+# 5 Schfunktionen
+# filter()  ->    herausgreifen von Werten
+# arrange() ->    anordnen
+# select()  ->    nach Variablennamen wählen
+# mutate()  ->    neue Variable mit Funktionen vorhandener erzeugen
+# summarize()->   viele Werte in kurzdarstellung zusammenfassen
+
+# group_by() ->   
+
+dplyr::filter(flights, month == 1, day ==1)
+# modifizierte Daten speichern
+
+jan1 <- filter(flights, month == 1, day ==1)
+
+ggplot(jan1) + geom_bar(mapping = aes(x = dep_time, fill = dep_time))
+
+################### Vergleiche ###################
+
+filter(flights, month == 1)
+# !! unterschiedliche Datentypen nicht miteinander vergleichen !!
+sqrt(2) ^2 == 2  # FALSE
+near (sqrt(2) ^2, 2)  # TRUE,
+
+### Boolsche Operatoren
+# oder | , und &, not ! -> ergeben Daten
+### Logische Verknüpfungen
+# oder ||, und &&, gleich == -> ergeben TRUE oder FALSE
+
+
+filter (flights, month == 12 | month == 11)
+### Inhaltsvergleich / Schnittmenge
+# %in%  x in y
+
+novdec <- filter(flights, month %in% c(11,12))
+filter(novdec, !(arr_delay > 120 | dep_delay > 120)) # entspricht
+filter(novdec, arr_delay <= 120 | dep_delay <= 120)
+x = NA
+y <- 5
+x == y
+is.na(novdec)
+### Fehlende Werte
+# bewahren der Fehlwerte bei filtern
+
+df <- tibble(x = c(1, NA, 3))
+fehlt <- filter(df, x > 1) ; fehlt # NA fehlt
+is_da <- filter(df, x > 1 | is.na(x)); is_da # NA ist da
+
+#Übung 1
+arr120 <- filter (flights, arr_delay >= 120); arr120
+dest <- filter (flights, dest == "IAH" | dest == "HOU"); dest
+airline <- filter (flights, carrier == "UA" |carrier == "AA" | carrier == "DL"); airline
+summer <- filter (flights, month > 6 & month < 10); summer
+dep120 <- filter (flights, dep_delay >= 120 & arr_delay <= 0); dep120
+arr1dep05 <- filter (flights, dep_delay >= 60 & (dep_delay - arr_delay >= 30)); arr1dep05
+dep0to6 <- filter (flights, dep_time >= 0 & dep_time <= 600); dep0to6
+
+#Übung 2
+?between
+bet0to6 <- filter (flights, between(dep_time, 0,  600)); bet0to6 # between nutzen
+#Übung 3
+depmiss <- filter (flights, is.na(dep_time)); nrow(depmiss) # 8255 mal
+varmiss <- filter (flights, is.na(dep_time)); varmiss # es fehlt auch ankunft und dauer stellt Fulgausfälle dar
+#Übung 4
+p <- NA ^ 0; is.na(p) # FALSE weil Wert hoch 0 immer 1 ist
+p
+
+### Zeilen mit arrange() anordnen
+arrange(flights, year, month, day)
+
